5966faebe08da581373d009bca7a4470d0488df3
Peter <Peter@mBp8qwRwro9Wq.local>
2017-11-09 08:18:52 +0100

    Repository change

---
+++ src/main/java/com/lwd/microBlog/MicroBlogApplication.java
@@ -0,0 +1,12 @@
+package com.lwd.microBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBlogApplication.class, args);
+	}
+}
