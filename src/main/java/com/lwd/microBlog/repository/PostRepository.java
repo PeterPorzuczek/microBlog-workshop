804c2d9c66d1dd944aa9fd526b83ca859dea3abe
Peter <Peter@mBp8qwRwro9Wq.local>
2017-11-09 07:40:57 +0100

    Repository change

--- src/main/java/com/lwd/microBlog/repository/PostRepository.java
+++ src/main/java/com/lwd/microBlog/repository/PostRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lwd.microBlog.model.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Serializable> {
 
 	Post findOneById(Long id);
